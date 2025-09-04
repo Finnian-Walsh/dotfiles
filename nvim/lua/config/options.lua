a00f36c75ee6c4f5e331c135869fc98a3f5cf9ca
Finnian Walsh <finnian@pop-os.tail2fff98.ts.net>
2025-09-04 17:15:38 +0100

    chore(nvim): update config files and plugins
    - add toggle hidden keymap for oil config
    - use <leader>/ for live grep
    - use a single options.lua file for general stuff

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,34 @@
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+vim.keymap.set("n", "<leader>e", ":Ex<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "rust",
+  callback = function()
+    vim.keymap.set("n", "<LocalLeader>c", ":!cargo check<CR>", { buffer = 0, desc = "Cargo check command (debug)" })
+    vim.keymap.set("n", "<LocalLeader>cr", ":!cargo check --release<CR>", { buffer = 0, desc = "Cargo check command (release)" })
+    vim.keymap.set("n", "<LocalLeader>b", ":!cargo build<CR>", { buffer = 0, desc = "Cargo build command (debug)" })
+    vim.keymap.set("n", "<LocalLeader>br", ":!cargo build --release<CR>", { buffer = 0, desc = "Cargo build command (release)" })
+    vim.keymap.set("n", "<LocalLeader>r", ":!cargo run<CR>", { buffer = 0, desc = "Cargo run command (debug)" })
+    vim.keymap.set("n", "<LocalLeader>rr", ":!cargo run --release<CR>", { buffer = 0, desc = "Cargo run command (release)" })
+    vim.keymap.set("n", "<LocalLeader>fmt", function()
+      if vim.api.nvim_buf_get_option(0, "modified") then
+        error("Open files have changes")
+      end
+
+      vim.cmd("!cargo fmt")
+      vim.cmd("edit")
+    end, { noremap = true, silent = true })
+  end,
+})
+
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,8 +8,7 @@ return {
       }
     }
     local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<Leader>ff", builtin.find_files, { desc = "Find files with telescope" })
-    vim.keymap.set("n", "<Leader>fg", builtin.live_grep, { desc = "Live grep with telescope" })
+    vim.keymap.set("n", "<Leader>/", builtin.live_grep, { desc = "Live grep with telescope" })
   end,
   lazy = false,
 }
