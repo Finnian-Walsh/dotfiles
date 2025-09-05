063c85a56b6869590ea45e5836c0008f4f1e7b0d
Finnian Walsh <finnian@pop-os.tail2fff98.ts.net>
2025-09-05 09:52:34 +0100

    chore(nvim): update lsp config
    - move mason code into lsp config file

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,16 +1,35 @@
 return {
-  "neovim/nvim-lspconfig",
-  config = function()    
-    local lspconfig = require("lspconfig")
-    lspconfig.rust_analyzer.setup{
-    	settings = {
-    		["rust-analyzer"] = {
-    			cargo = { allFeatures = true },
-          checkOnSave = true,
-    			check = { command = "clippy" },
-    		},
-    	}
-    }
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup{}
+    end,
+  }, {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup{
+        ensure_installed = { "rust_analyzer", "lua_ls", },
+      }
+    end,
+  }, {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.rust_analyzer.setup{
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = { allFeatures = true },
+            checkOnSave = true,
+            check = { command = "clippy" },
+          },
+        }
+      }
 
-  end,
+      lspconfig.lua_ls.setup{}
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show lsp documentation" })
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to lsp definition" })
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to lsp declaration" })
+      vim.keymap.set("n", "ca", vim.lsp.buf.code_action, { desc = "Lsp code actions" })
+    end,
+  },
 }
