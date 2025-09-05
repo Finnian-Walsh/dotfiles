348e3dfdd55a0cd8be792fc2a4b89feb096cbcab
Finnian Walsh <finnian@pop-os.tail2fff98.ts.net>
2025-09-05 12:39:27 +0100

    chore(nvim): update size keymaps
    - use arrow keys for resizing instead of hjkl
    - add window equalizer keymap

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -12,10 +12,11 @@ vim.g.maplocalleader = ","
 vim.keymap.set("n", "<leader>e", ":Ex<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { noremap = true, silent = true })
 
-vim.keymap.set("n", "<M-j>", ":resize +1<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<M-k>", ":resize +1<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<M-l>", ":vertical resize +1<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<M-h>", ":vertical resize -1<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-Up>", ":resize +1<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-Down>", ":resize -1<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-Right>", ":vertical resize +1<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-Left>", ":vertical resize -1<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-=>", ":wincmd =<CR>", { noremap = true, silent = true })
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "rust",
