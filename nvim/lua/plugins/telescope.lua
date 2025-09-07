666ce23a3f8dc2fde1a9bdf7005c691bbed49f1a
Finnian@arch <walshfinnian@gmail.com>
2025-09-07 20:49:40 +0100

    Update neovim config
    Implement a warning for the neovim telescope plugin for when ripgrep is unavailable

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,6 +7,11 @@ return {
         
       }
     }
+
+    if vim.fn.executable("rg") == 0 then
+      print("Warning: ripgrep is not available, so live grep will not work")
+    end
+
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<Leader>/", builtin.live_grep, { desc = "Live grep with telescope" })
   end,
