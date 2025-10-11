5c35904cca35710d229311a4e5e4c5e022bd7e99
Finnian@arch <walshfinnian@gmail.com>
2025-10-11 11:30:29 +0100

    chore(nvim): update config and indent code
    - add terminal escape keymap
    - use ` instead of c for cargo.toml viewing
    - update lsp configuration
    - indent lspconfig code correctly

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,45 +1,46 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup{}
-    end,
-  }, {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup{
-        ensure_installed = { "rust_analyzer", "lua_ls", },
-      }
-    end,
-  }, {
-    "neovim/nvim-lspconfig",
-    config = function()
-      vim.lsp.config("rust_analyzer", {
-        settings = {
-          ["rust-analyzer"] = {
-            cargo = { allFeatures = true },
-            checkOnSave = true,
-            check = { command = "clippy" },
-          },
-        }
-      })
-
-      vim.lsp.config("lua_ls", {
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = {
-                "vim",
-              }
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup{}
+        end,
+    }, {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup{
+                ensure_installed = { "rust_analyzer", "lua_ls", },
             }
-          }
-        }
-      })
+        end,
+    }, {
+        "neovim/nvim-lspconfig",
+        config = function()
+            vim.lsp.config("rust_analyzer", {
+                settings = {
+                    ["rust-analyzer"] = {
+                        cargo = { allFeatures = true },
+                        checkOnSave = true,
+                        check = { command = "clippy" },
+                    },
+                }
+            })
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show lsp documentation" })
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to lsp definition" })
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to lsp declaration" })
-      vim.keymap.set("n", "ca", vim.lsp.buf.code_action, { desc = "Lsp code actions" })
-    end,
-  },
+            vim.lsp.config("lua_ls", {
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = {
+                                "vim",
+                            }
+                        }
+                    }
+                }
+            })
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show lsp documentation" })
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to lsp definition" })
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to lsp declaration" })
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Lsp code actions" })
+            vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end)
+        end,
+    },
 }
