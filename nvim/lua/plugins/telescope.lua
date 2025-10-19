42d5a872e185b391c19d39d7d553ded955c33f90
Finnian@arch <walshfinnian@gmail.com>
2025-10-19 09:13:02 +0100

    chore(nvim): update plugin config
    - increase laziness of harpoon
    - reindent telescope configuration

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,19 +1,20 @@
 return {
-  "nvim-telescope/telescope.nvim", tag = "0.1.8",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("telescope").setup{
-      defaults = {
-        
-      }
-    }
+    "nvim-telescope/telescope.nvim", tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = { "<leader>/" },
+    config = function()
+        require("telescope").setup{
+            defaults = {
 
-    if vim.fn.executable("rg") == 0 then
-      print("Warning: ripgrep is not available, so live grep will not work")
-    end
+            }
+        }
 
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<Leader>/", builtin.live_grep, { desc = "Live grep with telescope" })
-  end,
-  lazy = false,
+        if vim.fn.executable("rg") == 0 then
+            print("Warning: ripgrep is not available, so live grep will not work")
+        end
+
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Live grep with telescope" })
+    end,
+    lazy = true,
 }
