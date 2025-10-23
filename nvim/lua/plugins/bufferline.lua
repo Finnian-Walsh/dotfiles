9a7735ddaa1a8c3f82d585d0834cf83f635b1bf3
Finnian@arch <walshfinnian@gmail.com>
2025-10-23 19:38:33 +0100

    chore(nvim): update bufferline config
    - prevent bufferline from displaying nameless buffers

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -3,6 +3,12 @@ return {
     version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
-        require("bufferline").setup{}
+        require("bufferline").setup{
+            options = {
+                custom_filter = function(bufnr)
+                    return vim.fn.bufname(bufnr) ~= ""
+                end,
+            },
+        }
     end,
 }
