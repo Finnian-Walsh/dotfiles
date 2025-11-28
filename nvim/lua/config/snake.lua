d6cf051350f02116ecd6e8c9ddb3e1a840c19151
finnian@penguin <walshfinnian@gmail.com>
2025-11-28 12:19:19 +0000

    chore(nvim): update snake
    - remove any recursion
    - handle arguments

--- nvim/lua/config/snake.lua
+++ nvim/lua/config/snake.lua
@@ -1,7 +1,9 @@
+local unpack = table.unpack or unpack
+
 local DirectionValue = {}
 
 function DirectionValue.new(identifier, advance, invert)
-    local self = setmetatable({
+    return setmetatable({
         _identifier = identifier,
         advance = advance,
         invert = invert,
@@ -12,13 +14,9 @@ function DirectionValue.__eq(this, other)
     return this._identifier == other._identifier
 end
 
-local Direction = setmetatable({}, {
-    __newindex = function()
-        error("Direction enum is read-only")
-    end
-})
+local Direction = {}
 
-Direction.Left = DirectionValue(
+Direction.Left = DirectionValue.new(
     1,
     function(pos)
         return { pos[1] - 1, pos[2] }
@@ -28,7 +26,7 @@ Direction.Left = DirectionValue(
     end
 )
 
-Direction.Down = DirectionValue(
+Direction.Down = DirectionValue.new(
     2,
     function(pos)
         return { pos[1], pos[2] + 1 }
@@ -38,7 +36,7 @@ Direction.Down = DirectionValue(
     end
 )
 
-Direction.Up = DirectionValue(
+Direction.Up = DirectionValue.new(
     3,
     function(pos)
         return { pos[1], pos[2] - 1 }
@@ -48,7 +46,7 @@ Direction.Up = DirectionValue(
     end
 )
 
-Direction.Right = DirectionValue(
+Direction.Right = DirectionValue.new(
     4,
     function(pos)
         return { pos[1] + 1, pos[2] }
@@ -58,21 +56,60 @@ Direction.Right = DirectionValue(
     end
 )
 
+setmetatable(Direction, {
+    __newindex = function()
+        error("Direction enum is read-only")
+    end
+})
+
 local Block = {}
 Block.__index = {}
 
+function Block.new(x, y, prev, next)
+    return setmetatable({
+        prev = prev,
+        next = next,
+        position = { x, y },
+    }, Block)
+end
 
+local function snake(opts)
+    local args = opts.fargs
 
-local function snake(args)
-    local difficulty
+    if #args > 2 then
+        vim.api.nvim_echo({
+            { "Unknown option: " .. args[3], "ErrorMsg" },
+        }, true, {})
+    end
+
+    local difficulty = args[1] or "normal"
+    local game_size = args[2] or "medium"
+
+    local waiting_time_ms
+
+    if difficulty == "easy" then
+        waiting_time_ms = 800
+    elseif difficulty == "normal" then
+        waiting_time_ms = 600
+    elseif difficulty == "hard" then
+        waiting_time_ms = 400
+    end
+
+    local game_width, game_height
+
+    if game_size == "small" then
+        game_width, game_height = 10, 10
+    elseif game_size == "medium" then
+        game_width, game_height = 15, 15
+    elseif game_size == "large" then
+        game_width, game_height = 20, 20
+    end
 
     local buf = vim.api.nvim_create_buf(false, true) -- listed, scratch
     vim.api.nvim_set_current_buf(buf)
 
     -- use a table for the snake body
     -- change the direction when keys are pressed
-
-    math.randomseed(os.time())
 
     local direction = Direction.Right
     local buffer_local = { buffer = buf }
@@ -112,31 +149,23 @@ local function snake(args)
     vim.keymap.set("n", "<Up>", turn_up, buffer_local)
     vim.keymap.set("n", "<Right>", turn_right, buffer_local)
 
-    local function prev_block(block, dir, count)
-        count = count or 1
-
-        local new_block = dir.advance(block)
-        new_block.next = block
-
-        if count > 1 then
-            block.prev = prev_block(block, dir, count - 1)
-        end
-
-        return new_block
-    end
-
-    local opposite_dir = direction:invert()
-    local tail = Block.new(5, 5, direction)
     local BODY_LENGTH = 3
-    local current = tail
 
-    for i = 2, BODY_LENGTH do
-        local new = current:advance()
-        new.direction = current.direction
+    local opposite_dir = direction.invert()
+    local head = Block.new(5, 5)
+    local tail = head
+
+    for _ = 2, BODY_LENGTH do
+        local current = Block.new(unpack(opposite_dir.advance(tail.position)))
+        current.prev = tail
+        tail.next = current
+        tail = current
     end
 
+    math.randomseed(os.time())
 end
 
 vim.api.nvim_create_user_command("Snake", snake, {
     nargs = "*",
 })
+
