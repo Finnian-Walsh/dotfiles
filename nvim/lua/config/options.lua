46de369fa867a0182c52145cccebaa3fc3d0fcc8
finnian@penguin <walshfinnian@gmail.com>
2025-11-28 12:20:22 +0000

    feat(nvim): add more keymaps
    - add double leader mappings for going to individual tabs
    - add <leader>O for creating a tab to the left (mirror the normal o vs
    O)
    - add <leader>A mapping for toggling Alpha screen

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -12,7 +12,7 @@ vim.opt.cursorline = true
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
 vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { noremap = true, silent = true, desc = "Turn highlight off" })
 
-
+vim.keymap.set("n", "<leader>A", ":Alpha<CR>", { silent = true, desc = "Toggle Alpha" })
 vim.keymap.set("n", "<leader>b", function()
     vim.opt.showtabline = vim.opt.showtabline:get() == 0 and 2 or 0
 end, { noremap = true, silent = true, desc = "Toggle bufferline" })
@@ -22,10 +22,33 @@ vim.keymap.set("n", "<leader>c", ":bd<CR>", { noremap = true, silent = true, des
 vim.keymap.set("n", "<leader><leader>c", ":bufdo bd<CR>", { noremap = true, silent = true, desc = "Close buffer"})
 
 vim.keymap.set("n", "<leader>o", ":tabnew<CR>", { noremap = true, silent = true, desc = "Open a new tab" })
+vim.keymap.set("n", "<leader>O", ":tabnew | tabmove -1<CR>", { noremap = true, silent = true, desc = "Open a new tab" })
 vim.keymap.set("n", "<leader>N", ":tabnext<CR>", { noremap = true, silent = true, desc = "Next buffer " })
 vim.keymap.set("n", "<leader>P", ":tabprev<CR>", { noremap = true, silent = true, desc = "Previous buffer "})
 vim.keymap.set("n", "<leader>C", ":tabclose<CR>", { noremap = true, silent = true, desc = "Close all buffers"})
 vim.keymap.set("n", "<leader><leader>C", ":tabonly<CR>", { noremap = true, silent = true, desc = "Close all buffers"})
+
+vim.keymap.set("n", "<leader><leader>1", ":tabnext 1<CR>", { noremap = true, silent = true, desc = "Go to tab 1" })
+vim.keymap.set("n", "<leader><leader>2", ":tabnext 2<CR>", { noremap = true, silent = true, desc = "Go to tab 2" })
+vim.keymap.set("n", "<leader><leader>3", ":tabnext 3<CR>", { noremap = true, silent = true, desc = "Go to tab 3" })
+vim.keymap.set("n", "<leader><leader>4", ":tabnext 4<CR>", { noremap = true, silent = true, desc = "Go to tab 4" })
+vim.keymap.set("n", "<leader><leader>5", ":tabnext 5<CR>", { noremap = true, silent = true, desc = "Go to tab 5" })
+vim.keymap.set("n", "<leader><leader>6", ":tabnext 6<CR>", { noremap = true, silent = true, desc = "Go to tab 6" })
+vim.keymap.set("n", "<leader><leader>7", ":tabnext 7<CR>", { noremap = true, silent = true, desc = "Go to tab 7" })
+vim.keymap.set("n", "<leader><leader>8", ":tabnext 8<CR>", { noremap = true, silent = true, desc = "Go to tab 8" })
+vim.keymap.set("n", "<leader><leader>9", ":tabnext 9<CR>", { noremap = true, silent = true, desc = "Go to tab 9" })
+vim.keymap.set("n", "<leader><leader>0", ":tabnext 0<CR>", { noremap = true, silent = true, desc = "Go to tab 0" })
+
+vim.keymap.set("n", "<leader><leader>!", ":tabnext 11<CR>", { noremap = true, silent = true, desc = "Go to tab 11" })
+vim.keymap.set("n", "<leader><leader>\"", ":tabnext 12<CR>", { noremap = true, silent = true, desc = "Go to tab 12" })
+vim.keymap.set("n", "<leader><leader>£", ":tabnext 13<CR>", { noremap = true, silent = true, desc = "Go to tab 13" })
+vim.keymap.set("n", "<leader><leader>$", ":tabnext 14<CR>", { noremap = true, silent = true, desc = "Go to tab 14" })
+vim.keymap.set("n", "<leader><leader>%", ":tabnext 15<CR>", { noremap = true, silent = true, desc = "Go to tab 15" })
+vim.keymap.set("n", "<leader><leader>^", ":tabnext 16<CR>", { noremap = true, silent = true, desc = "Go to tab 16" })
+vim.keymap.set("n", "<leader><leader>&", ":tabnext 17<CR>", { noremap = true, silent = true, desc = "Go to tab 17" })
+vim.keymap.set("n", "<leader><leader>*", ":tabnext 18<CR>", { noremap = true, silent = true, desc = "Go to tab 18" })
+vim.keymap.set("n", "<leader><leader>(", ":tabnext 19<CR>", { noremap = true, silent = true, desc = "Go to tab 19" })
+vim.keymap.set("n", "<leader><leader>)", ":tabnext 20<CR>", { noremap = true, silent = true, desc = "Go to tab 20" })
 
 vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { silent = true, desc = "Open neo-tree" })
 vim.keymap.set("n", "<leader>m", function()
