db4e8005c2583e6541a0f25cd236bca5b129a76a
finnian@penguin <walshfinnian@gmail.com>
2025-11-29 10:29:33 +0000

    refactor(nvim): update config options
    - use <cmd> instead of :
    - add repeatability for some commands
    - remove silent flag for commands since <cmd> is used instead
    - use nav_keys global table instead of verbose repetitive code for tab
    navigation
    - improve buffer autodeletion (exclude buffers in floating windows)

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -11,46 +11,62 @@ vim.opt.cursorline = true
 vim.opt.mouse = ""
 
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
-vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { noremap = true, silent = true, desc = "Turn highlight off" })
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { noremap = true, desc = "Turn highlight off" })
+vim.keymap.set("n", "<leader>s", "<cmd>source<CR>", { desc = "Source Neovim config" })
 
-vim.keymap.set("n", "<leader>A", ":Alpha<CR>", { silent = true, desc = "Toggle Alpha" })
-vim.keymap.set("n", "<leader>L", ":Lazy<CR>", { silent = true, desc = "Open Lazy" })
-vim.keymap.set("n", "<leader>M", ":Mason<CR>", { silent = true, desc = "Open Mason" })
+vim.keymap.set("n", "<leader>A", "<cmd>Alpha<CR>", { desc = "Toggle Alpha" })
+vim.keymap.set("n", "<leader>L", "<cmd>Lazy<CR>", { desc = "Open Lazy" })
+vim.keymap.set("n", "<leader>M", "<cmd>Mason<CR>", { desc = "Open Mason" })
 
 vim.keymap.set("n", "<leader>b", function()
     vim.opt.showtabline = vim.opt.showtabline:get() == 0 and 2 or 0
-end, { noremap = true, silent = true, desc = "Toggle bufferline" })
-vim.keymap.set("n", "<leader>n", ":bnext<CR>", { noremap = true, silent = true, desc = "Next buffer " })
-vim.keymap.set("n", "<leader>p", ":bprev<CR>", { noremap = true, silent = true, desc = "Previous buffer "})
-vim.keymap.set("n", "<leader>c", ":bd<CR>", { noremap = true, silent = true, desc = "Close buffer"})
-vim.keymap.set("n", "<leader>C", ":bufdo bd<CR>", { noremap = true, silent = true, desc = "Close buffer"})
+end, { noremap = true, desc = "Toggle bufferline" })
+vim.keymap.set("n", "<leader>n", function()
+    vim.cmd("bnext " .. vim.v.count1)
+end, { noremap = true, desc = "Next buffer " })
+vim.keymap.set("n", "<leader>p", function()
+    vim.cmd("bprev " .. vim.v.count1)
+end, { noremap = true, desc = "Previous buffer "})
+vim.keymap.set("n", "<leader>c", "<cmd>bd<CR>", { noremap = true, desc = "Close buffer"})
+vim.keymap.set("n", "<leader>C", "<cmd>bufdo bd<CR>", { noremap = true, desc = "Close buffer"})
 
 vim.keymap.set("n", "<leader><Right>", function()
     for _ = 1, vim.v.count1 do
         vim.cmd("BufferLineMoveNext")
     end
-end, { silent = true, desc = "Move the buffer right" })
+end, { desc = "Move the buffer right" })
 
 vim.keymap.set("n", "<leader><Left>", function()
     for _ = 1, vim.v.count1 do
         vim.cmd("BufferLineMovePrev")
     end
-end, { silent = true, desc = "Move the buffer left" })
+end, { desc = "Move the buffer left" })
 
-vim.keymap.set("n", "<leader>o", ":tabnew<CR>", { noremap = true, silent = true, desc = "Open a new tab" })
-vim.keymap.set("n", "<leader>O", ":tabnew | tabmove -1<CR>", { noremap = true, silent = true, desc = "Open a new tab" })
+vim.keymap.set("n", "<leader>o", function()
+    for _ = 1, vim.v.count1 do
+        vim.cmd("tabnew")
+    end
+end, { noremap = true, desc = "Open a new tab" })
+
+vim.keymap.set("n", "<leader>O", function()
+    for _ = 1, vim.v.count1 do
+        vim.cmd("tabnew | tabmove -1")
+    end
+end, { noremap = true, desc = "Open a new tab" })
+
 vim.keymap.set("n", "<leader><leader>n", function()
     for _ = 1, vim.v.count1 do
         vim.cmd("tabnext")
     end
-end, { noremap = true, silent = true, desc = "Next tab " })
+end, { noremap = true, desc = "Next tab " })
 vim.keymap.set("n", "<leader><leader>p", function()
     for _ = 1, vim.v.count1 do
         vim.cmd("tabprev")
     end
-end, { noremap = true, silent = true, desc = "Previous tab "})
-vim.keymap.set("n", "<leader><leader>c", ":tabclose<CR>", { noremap = true, silent = true, desc = "Close all buffers"})
-vim.keymap.set("n", "<leader><leader>C", ":tabonly<CR>", { noremap = true, silent = true, desc = "Close all buffers"})
+end, { noremap = true, desc = "Previous tab "})
+
+vim.keymap.set("n", "<leader><leader>c", "<cmd>tabclose<CR>", { noremap = true, desc = "Close all buffers"})
+vim.keymap.set("n", "<leader><leader>C", "<cmd>tabonly<CR>", { noremap = true, desc = "Close all buffers"})
 
 local function current_tab_can_move(count)
     local tabs = vim.api.nvim_list_tabpages()
@@ -76,7 +92,7 @@ vim.keymap.set("n", "<leader><leader><Right>", function()
     else
         vim.api.nvim_echo({{ "Cannot move tab +" .. count, "ErrorMsg" }}, true, {})
     end
-end, { silent = true, desc = "Move the tab right" })
+end, { desc = "Move the tab right" })
 
 vim.keymap.set("n", "<leader><leader><Left>", function()
     local count = vim.v.count1
@@ -85,34 +101,21 @@ vim.keymap.set("n", "<leader><leader><Left>", function()
     else
         vim.api.nvim_echo({{ "Cannot move tab -" .. count, "ErrorMsg" }}, true, {})
     end
-end, { silent = true, desc = "Move the tab left" })
+end, { desc = "Move the tab left" })
 
 vim.keymap.set("n", "<leader>z", function()
     local count = vim.v.count1
     print(count)
-end, { silent = true, desc = "Test keymap" })
+end, { desc = "Test keymap" })
 
-vim.keymap.set("n", "<leader><leader>1", ":tabnext 1<CR>", { noremap = true, silent = true, desc = "Go to tab 1" })
-vim.keymap.set("n", "<leader><leader>2", ":tabnext 2<CR>", { noremap = true, silent = true, desc = "Go to tab 2" })
-vim.keymap.set("n", "<leader><leader>3", ":tabnext 3<CR>", { noremap = true, silent = true, desc = "Go to tab 3" })
-vim.keymap.set("n", "<leader><leader>4", ":tabnext 4<CR>", { noremap = true, silent = true, desc = "Go to tab 4" })
-vim.keymap.set("n", "<leader><leader>5", ":tabnext 5<CR>", { noremap = true, silent = true, desc = "Go to tab 5" })
-vim.keymap.set("n", "<leader><leader>6", ":tabnext 6<CR>", { noremap = true, silent = true, desc = "Go to tab 6" })
-vim.keymap.set("n", "<leader><leader>7", ":tabnext 7<CR>", { noremap = true, silent = true, desc = "Go to tab 7" })
-vim.keymap.set("n", "<leader><leader>8", ":tabnext 8<CR>", { noremap = true, silent = true, desc = "Go to tab 8" })
-vim.keymap.set("n", "<leader><leader>9", ":tabnext 9<CR>", { noremap = true, silent = true, desc = "Go to tab 9" })
-vim.keymap.set("n", "<leader><leader>0", ":tabnext 0<CR>", { noremap = true, silent = true, desc = "Go to tab 0" })
+_G.nav_keys = {
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+    "!", "\"", "£", "$", "%", "^", "&", "*", "(", ")"
+}
 
-vim.keymap.set("n", "<leader><leader>!", ":tabnext 11<CR>", { noremap = true, silent = true, desc = "Go to tab 11" })
-vim.keymap.set("n", "<leader><leader>\"", ":tabnext 12<CR>", { noremap = true, silent = true, desc = "Go to tab 12" })
-vim.keymap.set("n", "<leader><leader>£", ":tabnext 13<CR>", { noremap = true, silent = true, desc = "Go to tab 13" })
-vim.keymap.set("n", "<leader><leader>$", ":tabnext 14<CR>", { noremap = true, silent = true, desc = "Go to tab 14" })
-vim.keymap.set("n", "<leader><leader>%", ":tabnext 15<CR>", { noremap = true, silent = true, desc = "Go to tab 15" })
-vim.keymap.set("n", "<leader><leader>^", ":tabnext 16<CR>", { noremap = true, silent = true, desc = "Go to tab 16" })
-vim.keymap.set("n", "<leader><leader>&", ":tabnext 17<CR>", { noremap = true, silent = true, desc = "Go to tab 17" })
-vim.keymap.set("n", "<leader><leader>*", ":tabnext 18<CR>", { noremap = true, silent = true, desc = "Go to tab 18" })
-vim.keymap.set("n", "<leader><leader>(", ":tabnext 19<CR>", { noremap = true, silent = true, desc = "Go to tab 19" })
-vim.keymap.set("n", "<leader><leader>)", ":tabnext 20<CR>", { noremap = true, silent = true, desc = "Go to tab 20" })
+for i = 1, 20 do
+    vim.keymap.set("n", "<leader><leader>" .. nav_keys[i], "<cmd>tabnext " .. i .. "<CR>", { noremap = true, desc = "Go to tab " .. i})
+end
 
 vim.api.nvim_create_autocmd("BufHidden", {
     callback = function(args)
@@ -123,15 +126,33 @@ vim.api.nvim_create_autocmd("BufHidden", {
         end
 
         local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-        if #lines == 1 and lines[1] == "" then
-            vim.schedule(function()
-                vim.api.nvim_buf_delete(buf, { force = true })
-            end)
+        if #lines > 1 or lines[1] ~= "" then
+            return
         end
+
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+            if vim.api.nvim_win_get_buf(win) == buf then
+                if vim.api.nvim_win_get_config(win).relative ~= "" then -- buffer in a floating window
+                    return
+                end
+            end
+        end
+
+        vim.schedule(function()
+            vim.api.nvim_buf_delete(buf, { force = true })
+        end)
     end
 })
 
-vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { silent = true, desc = "Open neo-tree" })
+vim.api.nvim_create_autocmd("BufLeave", {
+    callback = function()
+        vim.schedule(function()
+            vim.cmd("redrawtabline")
+        end)
+    end
+})
+
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", { desc = "Open neo-tree" })
 vim.keymap.set("n", "<leader>m", function()
     if vim.g.syntax_on then
         vim.cmd("syntax off")
@@ -140,13 +161,13 @@ vim.keymap.set("n", "<leader>m", function()
         vim.cmd("syntax enable")
         vim.treesitter.start()
     end
-end, { noremap = true, silent = true, desc = "Toggle Linus syntax highlighting" })
+end, { noremap = true, desc = "Toggle Linus syntax highlighting" })
 
-vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { noremap = true, silent = true, desc = "Move to window left" })
-vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { noremap = true, silent = true, desc = "Move to window below" })
-vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { noremap = true, silent = true, desc = "Move to window above" })
-vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { noremap = true, silent = true, desc = "Move to window right" })
-vim.keymap.set("n", "<C-`>", ":wincmd =<CR>", { noremap = true, silent = true, desc = "Equalize windows" })
+vim.keymap.set("n", "<C-h>", "<cmd>wincmd h<CR>", { noremap = true, desc = "Move to window left" })
+vim.keymap.set("n", "<C-j>", "<cmd>wincmd j<CR>", { noremap = true, desc = "Move to window below" })
+vim.keymap.set("n", "<C-k>", "<cmd>wincmd k<CR>", { noremap = true, desc = "Move to window above" })
+vim.keymap.set("n", "<C-l>", "<cmd>wincmd l<CR>", { noremap = true, desc = "Move to window right" })
+vim.keymap.set("n", "<C-`>", "<cmd>wincmd =<CR>", { noremap = true, desc = "Equalize windows" })
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "*",
@@ -168,7 +189,7 @@ vim.api.nvim_create_autocmd("FileType", {
             }
         })
 
-        local opts = { silent = true, buffer = true }
+        local opts = { buffer = true }
 
         vim.keymap.set("n", "<localleader>ff", function()
             if vim.api.nvim_buf_get_option(0, "modified") then
