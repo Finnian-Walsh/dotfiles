c8f35ce1270eef44147bbfecfa1b507ae2755671
finnian@penguin <walshfinnian@gmail.com>
2025-11-29 10:30:07 +0000

    refactor(nvim): update harpoon keys
    - reduce verbosity of harpoon code by using nav_keys global table

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -63,26 +63,10 @@ return {
 
         vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Open Harpoon" })
 
-        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon 1" })
-        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon 2" })
-        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon 3" })
-        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon 4" })
-        vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "Harpoon 5" })
-        vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end, { desc = "Harpoon 6" })
-        vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end, { desc = "Harpoon 7" })
-        vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end, { desc = "Harpoon 8" })
-        vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end, { desc = "Harpoon 9" })
-        vim.keymap.set("n", "<leader>0", function() harpoon:list():select(10) end, { desc = "Harpoon 10" })
-
-        vim.keymap.set("n", "<leader>!", function() harpoon:list():select(11) end, { desc = "Harpoon 11" })
-        vim.keymap.set("n", "<leader>\"", function() harpoon:list():select(12) end, { desc = "Harpoon 12" })
-        vim.keymap.set("n", "<leader>£", function() harpoon:list():select(13) end, { desc = "Harpoon 13" })
-        vim.keymap.set("n", "<leader>$", function() harpoon:list():select(14) end, { desc = "Harpoon 14" })
-        vim.keymap.set("n", "<leader>%", function() harpoon:list():select(15) end, { desc = "Harpoon 15" })
-        vim.keymap.set("n", "<leader>^", function() harpoon:list():select(16) end, { desc = "Harpoon 16" })
-        vim.keymap.set("n", "<leader>&", function() harpoon:list():select(17) end, { desc = "Harpoon 17" })
-        vim.keymap.set("n", "<leader>*", function() harpoon:list():select(18) end, { desc = "Harpoon 18" })
-        vim.keymap.set("n", "<leader>(", function() harpoon:list():select(19) end, { desc = "Harpoon 19" })
-        vim.keymap.set("n", "<leader>)", function() harpoon:list():select(20) end, { desc = "Harpoon 20" })
+        for i = 1, 20 do
+            vim.keymap.set("n", "<leader>" .. nav_keys[i], function()
+                harpoon:list():select(i)
+            end, { desc = "Harpoon " .. i})
+        end
     end,
 }
