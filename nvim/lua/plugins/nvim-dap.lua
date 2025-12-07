806562946c42233ccc0ec40f17aa6efd5f2ddb72
finnian@penguin <walshfinnian@gmail.com>
2025-12-07 11:03:10 +0000

    feat(nvim): add python debugging

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -1,17 +1,27 @@
 return {
-    "mfussenegger/nvim-dap",
-    config = function()
-        local dap = require("dap")
-        vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
-        vim.keymap.set("n", "<leader>dr", dap.clear_breakpoints, { desc = "Toggle breakpoint" })
-        vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue debugging" })
-        vim.keymap.set("n", "<leader>dn", dap.run, { desc = "New debugging session" })
+    {
+        "mfussenegger/nvim-dap-python",
+        config = function()
+            require("dap-python").setup("python3")
+        end
+    },
+    {
+        "mfussenegger/nvim-dap",
 
-        vim.fn.sign_define("DapBreakpoint", {
-            text = "●",          -- symbol
-            texthl = "DapBreakpoint",
-        })
+        config = function()
+            local dap = require("dap")
 
-        vim.api.nvim_set_hl(0, "DapBreakpoint", { fg = "#FF0000", bg = "", bold = true })
-    end
+            vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+            vim.keymap.set("n", "<leader>dr", dap.clear_breakpoints, { desc = "Toggle breakpoint" })
+            vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue debugging" })
+            vim.keymap.set("n", "<leader>dn", "<cmd>DapNew<CR>", { desc = "New debugging session" })
+
+            vim.fn.sign_define("DapBreakpoint", {
+                text = "●",          -- symbol
+                texthl = "DapBreakpoint",
+            })
+
+            vim.api.nvim_set_hl(0, "DapBreakpoint", { fg = "#FF0000", bg = "", bold = true })
+        end
+    }
 }
