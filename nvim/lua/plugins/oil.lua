2fbdca53c576373d747b4f0af4980417c5547b4a
finnian@penguin <walshfinnian@gmail.com>
2025-12-07 16:04:25 +0000

    feat(nvim): extend oil keymaps
    - add oil keymaps for opening the oil buffer in a new window

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,7 +2,8 @@ return {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("oil").setup{
+        local oil = require("oil")
+        oil.setup{
             keymaps = {
                 ["<localleader>h"] = "actions.toggle_hidden",
                 ["<C-l>"] = false,
@@ -14,8 +15,31 @@ return {
             },
         }
 
-        vim.keymap.set("n", "<leader>t", require("oil").open, { desc = "Open oil file tree" })
-        vim.keymap.set("n", "<leader>T", function() require("oil").open(vim.uv.cwd()) end, { desc = "Open oil file tree at cwd" })
+        local function open_at_cwd()
+            oil.open(vim.uv.cwd())
+        end
+        vim.keymap.set("n", "<leader>t", oil.open, { desc = "Open oil file tree" })
+        vim.keymap.set("n", "<leader>T", open_at_cwd, { desc = "Open oil file tree at cwd" })
+
+        vim.keymap.set("n", "<leader>nt", function()
+            vim.cmd("vs | wincmd l")
+            oil.open()
+        end, { desc = "Open oil file tree in new vertical split" })
+
+        vim.keymap.set("n", "<leader>Nt", function()
+            vim.cmd("sp | wincmd j")
+            oil.open()
+        end, { desc = "Open oil file tree in new horizontal split" })
+
+        vim.keymap.set("n", "<leader>nT", function()
+            vim.cmd("vs | wincmd l")
+            open_at_cwd()
+        end, { desc = "Open oil file tree at cwd in new vertical split" })
+
+        vim.keymap.set("n", "<leader>NT", function()
+            vim.cmd("sp | wincmd j")
+            open_at_cwd()
+        end, { desc = "Open oil file tree at cwd in new horizontal split" })
     end,
     lazy = false,
 }
