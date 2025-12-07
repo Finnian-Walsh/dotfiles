8d17c696899f025e397ef33ed57b0f8d7692f27f
finnian@penguin <walshfinnian@gmail.com>
2025-12-07 10:26:16 +0000

    chore(nvim): update dap config
    - change the breakpoint text and highlight so that it looks nice

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -1,8 +1,17 @@
 return {
     "mfussenegger/nvim-dap",
     config = function()
-        vim.keymap.set("n", "<leader>db", "<cmd>DapToggleBreakpoint<CR>", { desc = "Toggle breakpoint" })
-        vim.keymap.set("n", "<leader>dc", "<cmd>DapContinue<CR>", { desc = "Continue debugging" })
-        vim.keymap.set("n", "<leader>dn", "<cmd>DapNew<CR>", { desc = "New debugging session" })
+        local dap = require("dap")
+        vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+        vim.keymap.set("n", "<leader>dr", dap.clear_breakpoints, { desc = "Toggle breakpoint" })
+        vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue debugging" })
+        vim.keymap.set("n", "<leader>dn", dap.run, { desc = "New debugging session" })
+
+        vim.fn.sign_define("DapBreakpoint", {
+            text = "●",          -- symbol
+            texthl = "DapBreakpoint",
+        })
+
+        vim.api.nvim_set_hl(0, "DapBreakpoint", { fg = "#FF0000", bg = "", bold = true })
     end
 }
