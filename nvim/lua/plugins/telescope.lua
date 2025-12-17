45cb3f8b2b45d3d1287fc1741867fca37ef66eaf
finnian@penguin <walshfinnian@gmail.com>
2025-12-17 20:06:10 +0000

    feat(nvim): add telescope keymaps
    - add leader b/ for buffer searching (start in insert mode)
    - add leader b? for buffer viewing (start in normal mode)

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -15,11 +15,17 @@ return {
 
         local initial_mode_normal = { initial_mode = "normal" }
         vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Live grep with telescope" })
+
         vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Find files with telescope"})
         vim.keymap.set("n", "<leader>F", function() builtin.find_files(initial_mode_normal) end, { desc = "Find files with telescope"})
+
         vim.keymap.set("n", "<leader>R", builtin.resume, { desc = "Resume previous telescope action" })
         vim.keymap.set("n", "<leader>D", builtin.diagnostics, { desc = "View diagnostics" })
+
         vim.keymap.set("n", "<leader>C/", builtin.colorscheme, { desc = "Search for colorschemes" })
         vim.keymap.set("n", "<leader>C?", function() builtin.colorscheme(initial_mode_normal) end, { desc = "View colorschemes" })
+
+        vim.keymap.set("n", "<leader>b/", builtin.buffers, { desc = "Search for buffer with telescope" })
+        vim.keymap.set("n", "<leader>b?", function() builtin.buffers(initial_mode_normal) end, { desc = "View buffers with telescope" })
     end,
 }
