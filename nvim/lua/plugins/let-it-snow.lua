596ba4847a2f90be84be21f6952bbfe00d7baaec
walshfinnian <walshfinnian@penguin.(none)>
2025-12-30 11:56:17 +0000

    fix(nvim): snow keymap
    - use the snow command instead of function since the let-it-snow.nvim
    plugin is set to lazily load when the command is entered
    - rename regular expression variable to pattern in matched keys function

--- nvim/lua/plugins/let-it-snow.lua
+++ nvim/lua/plugins/let-it-snow.lua
@@ -5,11 +5,7 @@ end
 return {
     "marcussimonsen/let-it-snow.nvim",
     cmd = "LetItSnow",
-    config = function ()
-        local let_it_snow = require("let-it-snow")
-        let_it_snow.setup{
-            delay = 100,
-        }
-        vim.keymap.set("n", "<leader>S", let_it_snow.let_it_snow, { desc = "Let it snow!" })
-    end
+    opts = {
+        delay = 150,
+    },
 }
