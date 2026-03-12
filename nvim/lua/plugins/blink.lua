16ce90328a1d2b9e70ad76690f978187b20dd587
walshfinnian <walshfinnian@penguin.(none)>
2026-03-12 17:34:41 +0000

    chore(nvim): update blink.cmp config
    - use double quotations
    - add blink compat
    - add a keymaps for insert next and insert prev

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,27 +1,36 @@
 return {
-    'saghen/blink.cmp',
-    dependencies = { 'rafamadriz/friendly-snippets' },
-    event = "VeryLazy",
+    {
+        "saghen/blink.cmp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        event = "VeryLazy",
 
-    version = '*',
+        version = '*',
 
-    opts = {
-        keymap = {
-            ["<Tab>"] = { "select_next", "fallback" },
-            ["<S-Tab>"] = { "select_prev", "fallback" },
-            ["<CR>"] = { "select_and_accept", "fallback" },
-            ["<C-e>"] = { "hide", "fallback" },
-            -- <C-space> for documentation
+        opts = {
+            keymap = {
+                ["<Right>"] = { "insert_next", "fallback" },
+                ["<Left>"] = { "insert_prev", "fallback" },
+                ["<Tab>"] = { "select_next", "fallback" },
+                ["<S-Tab>"] = { "select_prev", "fallback" },
+                ["<CR>"] = { "select_and_accept", "fallback" },
+                ["<C-e>"] = { "hide", "fallback" },
+                -- <C-space> for documentation
+            },
+
+            completion = { documentation = { auto_show = false } },
+
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
+
+            fuzzy = { implementation = "prefer_rust_with_warning" }
         },
 
-        completion = { documentation = { auto_show = false } },
-
-        sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
-        },
-
-        fuzzy = { implementation = "prefer_rust_with_warning" }
+        opts_extend = { "sources.default" },
     },
-
-    opts_extend = { "sources.default" },
+    {
+        "saghen/blink.compat",
+        version = "*",
+        opts = {},
+    }
 }
