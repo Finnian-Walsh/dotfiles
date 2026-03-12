d55a4f9190cd3684b5f800e4a82b39fba12cf880
walshfinnian <walshfinnian@penguin.(none)>
2026-03-12 22:05:11 +0000

    feat(nvim): add todo comments keymaps
    - add [t for jump prev
    - add ]t for jump next
    - create two global functions for resetting and disabling todo signs

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -22,5 +22,25 @@ return {
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
         opts = {},
+        -- TODO: add the next thing to the neovim config
+        -- FIX: critical bug!
+        -- PERF: yes yes yes yes we have the best performance ever
+        -- WARNING: something very bad
+        -- NOTE: Neovim btw
+        config = function(_, opts)
+            local todo_comments = require("todo-comments")
+            todo_comments.setup(opts)
+
+            vim.keymap.set("n", "[t", todo_comments.jump_prev, { desc = "Jump to the previous todo comment" })
+            vim.keymap.set("n", "]t", todo_comments.jump_next, { desc = "Jump to the next todo comment" })
+
+            function _G.reset_todo_signs()
+                todo_comments.reset()
+            end
+
+            function _G.disable_todo_signs()
+                todo_comments.disable()
+            end
+        end,
     },
 }
