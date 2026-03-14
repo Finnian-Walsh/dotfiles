04eb3af796c297306c39ed933c2b7bfa8541fb81
walshfinnian <walshfinnian@penguin.(none)>
2026-03-14 10:55:13 +0000

    feat(nvim): extend dap config
    - add stepping keymaps
    - add a widgets keymap

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -8,21 +8,33 @@ return {
             vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
             vim.keymap.set("n", "<leader>dB", dap.clear_breakpoints, { desc = "Toggle breakpoint" })
             vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue debugging" })
-            vim.keymap.set("n", "<leader>dn", "<cmd>DapNew<CR>", { desc = "New debugging session" })
+            vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step into" })
+            vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "Step over" })
+            vim.keymap.set("n", "<leader>dO", dap.step_out, { desc = "Step out" })
+
+            local widgets
+
+            vim.keymap.set("n", "<leader>dw", function()
+                if not widgets then
+                    widgets = require("dap.ui.widgets")
+                end
+
+                local sidebar = widgets.sidebar(widgets.scopes)
+                sidebar.open()
+            end)
 
             vim.fn.sign_define("DapBreakpoint", {
-                text = "●",          -- symbol
+                text = "🔴", -- symbol
                 texthl = "DapBreakpoint",
             })
 
             vim.api.nvim_set_hl(0, "DapBreakpoint", { fg = "#FF0000", bg = "", bold = true })
-        end
+        end,
     },
     {
         "mfussenegger/nvim-dap-python",
         config = function()
             require("dap-python").setup("python3")
-        end
+        end,
     },
-
 }
