3b3a75ab542650e7223bcef2f3a9b06fa88e980a
walshfinnian <walshfinnian@penguin.(none)>
2026-05-10 14:43:25 +0100

    refactor(nvim): move post aesthetics code
    - move the post aesthetics code from the main init file into the
    aesthetics init file

--- nvim/lua/config/aesthetics/init.lua
+++ nvim/lua/config/aesthetics/init.lua
@@ -3,3 +3,5 @@ require("config.aesthetics.bufferline")
 require("config.aesthetics.colorschemes")
 require("config.aesthetics.let-it-snow")
 require("config.aesthetics.lualine")
+
+vim.opt.showtabline = 0
