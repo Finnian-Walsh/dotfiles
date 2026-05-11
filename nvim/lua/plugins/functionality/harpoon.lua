6bf0bab91bb589f418d3bc8b9067c4fe7488a160
walshfinnian <walshfinnian@penguin.(none)>
2026-05-11 16:34:37 -0400

    feat(nvim): replace harpoon plugin with hooker
    - use hooker.nvim for better control

--- nvim/lua/plugins/functionality/harpoon.lua
+++
@@ -1,7 +0,0 @@
-return {
-    {
-        src = "https://github.com/ThePrimeagen/harpoon",
-        version = "87b1a35",
-    },
-    "https://github.com/nvim-lua/plenary.nvim",
-}
---
+++ nvim/lua/plugins/functionality/hooker.lua
@@ -0,0 +1,3 @@
+return {
+    "https://github.com/Finnian-Walsh/hooker.nvim",
+}
