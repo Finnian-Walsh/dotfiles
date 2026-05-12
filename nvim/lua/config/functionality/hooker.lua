f78b80b3da12d6d81b470857f0cc0cb8a3fee72c
walshfinnian <walshfinnian@penguin.(none)>
2026-05-13 00:02:34 +0200

    fix(nvim): add bounds checking to hooker

--- nvim/lua/config/functionality/hooker.lua
+++ nvim/lua/config/functionality/hooker.lua
@@ -11,33 +11,66 @@ vim.keymap.set("n", "<leader>h", hooker.menu)
 for i = 1, 20 do
     local navigation_key = nav_keys[i]
 
-    -- local function warn_cannot_select()
-    --     vim.notify("There is no item " .. i .. " in the harpoon list", vim.log.levels.WARN)
-    -- end
+    local warning_message = "There is no item " .. i .. " in the hooker list"
 
-    vim.keymap.set("n", "<leader>" .. navigation_key, function()
-        hooker.select(i)
-    end, { desc = "Harpoon " .. i })
+    local function bound_function(f)
+        return function()
+            if hooker.length() >= i then
+                f()
+            else
+                vim.notify(warning_message, vim.log.levels.WARN)
+            end
+        end
+    end
 
-    vim.keymap.set("n", "<leader>n" .. navigation_key, function()
-        vim.cmd("vs | wincmd l")
-        hooker.select(i)
-    end, { desc = "Harpoon " .. i .. " (vertical split)" })
+    vim.keymap.set(
+        "n",
+        "<leader>" .. navigation_key,
+        bound_function(function()
+            hooker.select(i)
+        end),
+        { desc = "Harpoon " .. i }
+    )
 
-    vim.keymap.set("n", "<leader>N" .. navigation_key, function()
-        vim.cmd("sp | wincmd j")
-        hooker.select(i)
-    end, { desc = "Harpoon " .. i .. " (horizontal split)" })
+    vim.keymap.set(
+        "n",
+        "<leader>n" .. navigation_key,
+        bound_function(function()
+            vim.cmd("vs | wincmd l")
+            hooker.select(i)
+        end),
+        { desc = "Harpoon " .. i .. " (vertical split)" }
+    )
 
-    vim.keymap.set("n", "<leader>o" .. navigation_key, function()
-        hooker.select(i)
-        vim.cmd("BufOnly")
-    end, {
-        desc = "Harpoon switch to only " .. i .. " and displayed",
-    })
+    vim.keymap.set(
+        "n",
+        "<leader>N" .. navigation_key,
+        bound_function(function()
+            vim.cmd("sp | wincmd j")
+            hooker.select(i)
+        end),
+        { desc = "Harpoon " .. i .. " (horizontal split)" }
+    )
 
-    vim.keymap.set("n", "<leader>O" .. navigation_key, function()
-        hooker.select(i)
-        vim.cmd("BufCurrentOnly")
-    end, { desc = "Harpoon switch to only " .. i })
+    vim.keymap.set(
+        "n",
+        "<leader>o" .. navigation_key,
+        bound_function(function()
+            hooker.select(i)
+            vim.cmd.BufOnly()
+        end),
+        {
+            desc = "Harpoon switch to only " .. i .. " and displayed",
+        }
+    )
+
+    vim.keymap.set(
+        "n",
+        "<leader>O" .. navigation_key,
+        bound_function(function()
+            hooker.select(i)
+            vim.cmd.BufCurrentOnly()
+        end),
+        { desc = "Harpoon switch to only " .. i }
+    )
 end
