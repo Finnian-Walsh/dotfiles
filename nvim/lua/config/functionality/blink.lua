630a098f27f8b78fd4d9ac1dd0b4823d9318ab86
walshfinnian <walshfinnian@penguin.(none)>
2026-05-13 22:01:53 +0200

    perf(nvim): defer blink setup

--- nvim/lua/config/functionality/blink.lua
+++ nvim/lua/config/functionality/blink.lua
@@ -1,4 +1,5 @@
 local blink_cmp = require("blink.cmp")
+
 local build_task = blink_cmp.build()
 
 build_task:on_reject(function()
@@ -6,23 +7,25 @@ build_task:on_reject(function()
 end)
 
 build_task:on_resolve(function()
-    blink_cmp.setup {
-        keymap = {
-            ["<Right>"] = { "insert_next", "fallback" },
-            ["<Left>"] = { "insert_prev", "fallback" },
-            ["<Tab>"] = { "select_next", "fallback" },
-            ["<S-Tab>"] = { "select_prev", "fallback" },
-            ["<CR>"] = { "select_and_accept", "fallback" },
-            ["<C-e>"] = { "hide", "fallback" },
-            -- <C-space> for documentation
-        },
+    vim.schedule(function()
+        blink_cmp.setup {
+            keymap = {
+                ["<Right>"] = { "insert_next", "fallback" },
+                ["<Left>"] = { "insert_prev", "fallback" },
+                ["<Tab>"] = { "select_next", "fallback" },
+                ["<S-Tab>"] = { "select_prev", "fallback" },
+                ["<CR>"] = { "select_and_accept", "fallback" },
+                ["<C-e>"] = { "hide", "fallback" },
+                -- <C-space> for documentation
+            },
 
-        completion = { documentation = { auto_show = false } },
+            completion = { documentation = { auto_show = false } },
 
-        sources = {
-            default = { "lsp", "path", "snippets", "buffer" },
-        },
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer" },
+            },
 
-        fuzzy = { implementation = "prefer_rust_with_warning" },
-    }
+            fuzzy = { implementation = "prefer_rust_with_warning" },
+        }
+    end)
 end)
