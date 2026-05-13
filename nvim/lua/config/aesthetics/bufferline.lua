1d4598c05fc4834f9781bfc830fa05a856dd269b
walshfinnian <walshfinnian@penguin.(none)>
2026-05-13 22:02:26 +0200

    perf(nvim): lazy load bufferline
    - make bufferline load on UIEnter
    - set showtabline to 0 after calling setup

--- nvim/lua/config/aesthetics/bufferline.lua
+++ nvim/lua/config/aesthetics/bufferline.lua
@@ -1,14 +1,3 @@
-require("bufferline").setup {
-    options = {
-        custom_filter = function(buf)
-            local buf_info = vim.bo[buf]
-            local filetype = buf_info.filetype
-            local is_normal = filetype ~= "alpha" and filetype ~= "harpoon"
-            return is_normal and (vim.api.nvim_buf_get_name(buf) ~= "" or buf_info.modified)
-        end,
-    },
-}
-
 local function show_tabline()
     vim.o.showtabline = 2
 end
@@ -28,3 +17,20 @@ end
 vim.keymap.set("n", "<leader>B", toggle_bufferline, { noremap = true, desc = "Toggle bufferline" })
 
 vim.api.nvim_create_user_command("ToggleBufferline", toggle_bufferline, { desc = "Toggle bufferline" })
+
+vim.api.nvim_create_autocmd("UIEnter", {
+    callback = function()
+        require("bufferline").setup {
+            options = {
+                custom_filter = function(buf)
+                    local buf_info = vim.bo[buf]
+                    local filetype = buf_info.filetype
+                    local is_normal = filetype ~= "alpha" and filetype ~= "harpoon"
+                    return is_normal and (vim.api.nvim_buf_get_name(buf) ~= "" or buf_info.modified)
+                end,
+            },
+        }
+
+        hide_tabline()
+    end,
+})
