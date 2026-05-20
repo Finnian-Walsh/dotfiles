f4644428686a17f8a50ad5c2cef5dd0a466a8b91
walshfinnian <walshfinnian@penguin.(none)>
2026-05-20 22:17:20 +0100

    feat(nvim)!: replace neo-tree with mini.files
    - use <leader>t for mini.files and <leader>T for oil
    - remove neo-tree

--- nvim/lua/plugins/functionality/editor.lua
+++ nvim/lua/plugins/functionality/editor.lua
@@ -2,6 +2,7 @@ return {
     "https://github.com/kylechui/nvim-surround",
     "https://github.com/windwp/nvim-autopairs",
     "https://github.com/nvim-mini/mini.ai",
+    "https://github.com/nvim-mini/mini.files",
     "https://github.com/folke/todo-comments.nvim",
     "https://github.com/nvim-lua/plenary.nvim",
 }
