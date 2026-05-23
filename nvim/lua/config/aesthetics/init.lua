a517176eee996dcea3f0f0f3dd46cb0c9ea27f25
walshfinnian <walshfinnian@penguin.(none)>
2026-05-23 18:57:07 +0100

    refactor(nvim): move lualine setup to init
    Move the setup call to lualine to the init file of the aesthetics plugin
    configurations

--- nvim/lua/config/aesthetics/init.lua
+++ nvim/lua/config/aesthetics/init.lua
@@ -1,6 +1,6 @@
 require("config.aesthetics.alpha")
 require("config.aesthetics.bufferline")
 require("config.aesthetics.let-it-snow")
-require("config.aesthetics.lualine")
+require("lualine").setup {}
 
 vim.opt.showtabline = 0
