5af8113c468408b817080cc1abff6afdd52f7255
walshfinnian <walshfinnian@penguin.(none)>
2026-06-03 22:39:40 +0200

    perf(nvim): make toggleterm load lazily

--- nvim/lua/config/functionality/toggleterm.lua
+++ nvim/lua/config/functionality/toggleterm.lua
@@ -1,3 +1,8 @@
-require("toggleterm").setup {}
+local toggleterm_keymap = "<leader><CR>"
+local toggleterm_opts = { desc = "Toggle terminal" }
 
-vim.keymap.set("n", "<leader><CR>", vim.cmd.ToggleTerm, { desc = "Toggle terminal" })
+vim.keymap.set("n", toggleterm_keymap, function()
+    require("toggleterm").setup {}
+    vim.keymap.set("n", toggleterm_keymap, vim.cmd.ToggleTerm, toggleterm_opts)
+    vim.cmd.ToggleTerm()
+end, toggleterm_opts)
