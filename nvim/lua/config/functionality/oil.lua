998e4f5a5eb5fcc9b5cce0c218dcf51021f7a3b2
walshfinnian <walshfinnian@penguin.(none)>
2026-06-03 22:33:47 +0200

    feat(nvim): add oil cwd keymaps

--- nvim/lua/config/functionality/oil.lua
+++ nvim/lua/config/functionality/oil.lua
@@ -23,3 +23,18 @@ vim.keymap.set("n", "<leader>Ne", function()
     vim.cmd("sp | wincmd j")
     oil.open()
 end, { desc = "Open oil file tree in new horizontal split" })
+
+local function open_at_cwd()
+    oil.open(vim.uv.cwd())
+end
+vim.keymap.set("n", "<leader>E", open_at_cwd, { desc = "Open oil file tree" })
+
+vim.keymap.set("n", "<leader>nE", function()
+    vim.cmd("sp | wincmd j")
+    open_at_cwd()
+end)
+
+vim.keymap.set("n", "<leader>NE", function()
+    vim.cmd("vs | wincmd l")
+    open_at_cwd()
+end)
