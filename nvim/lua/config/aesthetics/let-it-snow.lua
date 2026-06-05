8356b81f7705705b26bf13287e423496a3ea183b
walshfinnian <walshfinnian@penguin.(none)>
2026-06-05 14:52:05 +0100

    chore(nvim): use lazy keymaps for let-it-snow.nvim

--- nvim/lua/config/aesthetics/let-it-snow.lua
+++ nvim/lua/config/aesthetics/let-it-snow.lua
@@ -1,12 +1,18 @@
+local keymaps = require("core.lazy_keymaps").new(function()
+    require("let-it-snow").setup {
+        delay = 150,
+    }
+end)
+
+keymaps:add("n", "<leader>S", function()
+    return vim.cmd.LetItSnow
+end, { desc = "Let it snow!" })
+
+keymaps:cmd("LetItSnow")
+
 if current_month ~= 12 then -- only necessary during December
     return
 end
-
-require("let-it-snow").setup {
-    delay = 150,
-}
-
-vim.keymap.set("n", "<leader>S", vim.cmd.LetItSnow, { desc = "Let it snow!" })
 
 vim.api.nvim_create_autocmd("BufEnter", {
     callback = function(_)
