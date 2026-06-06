cf6d613465bb469c202b15ff9cbcb8f0363c2d92
walshfinnian <walshfinnian@penguin.(none)>
2026-06-06 16:06:01 +0100

    refactor(nvim): update plugin addition interface
    Create an add function for extending the plugin list

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,108 +1,112 @@
 local plugins = {}
 
-vim.list_extend(plugins, {
+local function add(additional_plugins)
+    vim.list_extend(plugins, additional_plugins)
+end
+
+add {
     "https://github.com/ChaseRensberger/christmas.nvim",
     "https://github.com/folke/tokyonight.nvim",
     { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
     "https://github.com/ellisonleao/gruvbox.nvim",
-})
+}
 
-vim.list_extend(plugins, {
+add {
     "https://github.com/goolord/alpha-nvim",
     "https://github.com/nvim-tree/nvim-web-devicons",
-})
+}
 
-vim.list_extend(plugins, {
+add {
     "https://github.com/akinsho/bufferline.nvim",
     "https://github.com/nvim-tree/nvim-web-devicons",
-})
+}
 
-vim.list_extend(plugins, {
+add {
     "https://github.com/marcussimonsen/let-it-snow.nvim",
-})
+}
 
-vim.list_extend(plugins, {
-    "https://github.com/nvim-tree/nvim-web-devicons",
+add {
     "https://github.com/nvim-lualine/lualine.nvim",
-})
+    "https://github.com/nvim-tree/nvim-web-devicons",
+}
 
-vim.list_extend(plugins, {
+add {
+    "https://github.com/saghen/blink.cmp",
     "https://github.com/saghen/blink.lib",
     "https://github.com/rafamadriz/friendly-snippets",
-    "https://github.com/saghen/blink.cmp",
     "https://github.com/saghen/blink.compat",
-})
+}
 
-vim.list_extend(plugins, {
+add {
     "https://github.com/windwp/nvim-autopairs",
     "https://github.com/nvim-mini/mini.files",
     "https://github.com/nvim-mini/mini.surround",
     "https://github.com/folke/todo-comments.nvim",
     "https://github.com/nvim-lua/plenary.nvim",
-})
+}
 
-vim.list_extend(plugins, {
+add {
     "https://github.com/lewis6991/gitsigns.nvim",
-})
+}
 
-vim.list_extend(plugins, {
+add {
     "https://github.com/Finnian-Walsh/hooker.nvim",
-})
+}
 
-vim.list_extend(plugins, {
+add {
     "https://github.com/mason-org/mason.nvim",
     "https://github.com/mason-org/mason-lspconfig.nvim",
     "https://github.com/neovim/nvim-lspconfig",
     "https://github.com/stevearc/conform.nvim",
     "https://github.com/mrcjkb/rustaceanvim",
     "https://github.com/neovim/nvim-lspconfig",
-})
+}
 
-vim.list_extend(plugins, {
+add {
     "https://github.com/nvim-neorg/neorg",
-})
+}
 
-vim.list_extend(plugins, {
+add {
     "https://github.com/mfussenegger/nvim-dap",
     "https://github.com/mfussenegger/nvim-dap-python",
-})
+}
 
-vim.list_extend(plugins, {
+add {
     "https://github.com/stevearc/oil.nvim",
     "https://github.com/nvim-tree/nvim-web-devicons",
-})
+}
 
-vim.list_extend(plugins, {
+add {
     { src = "https://github.com/Finnian-Walsh/plugin-view.nvim", version = "feat/buffer-updation" },
-})
+}
 
-vim.list_extend(plugins, {
+add {
     "https://github.com/nvim-telescope/telescope.nvim",
     "https://github.com/nvim-lua/plenary.nvim",
     "https://github.com/nvim-telescope/telescope-fzf-native.nvim",
-})
+}
 
-vim.list_extend(plugins, {
+add {
     "https://github.com/akinsho/toggleterm.nvim",
-})
+}
 
-vim.list_extend(plugins, {
+add {
     "https://github.com/mbbill/undotree",
-})
+}
 
-vim.list_extend(plugins, {
+add {
     "https://github.com/folke/which-key.nvim",
-})
+}
 
-vim.list_extend(plugins, {
+add {
     "https://github.com/pogyomo/winresize.nvim",
-})
+}
 
--- vim.list_extend(plugins, {
+-- add {
 --     {
 --         src = "https://github.com/Finnian-Walsh/plugin-testing.nvim",
 --         version = "✨",
 --     },
--- })
+-- }
 
 return plugins
