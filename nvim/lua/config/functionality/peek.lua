57691cd3bc34e2aa9fde40b5aabc048c4fc9d766
walshfinnian <walshfinnian@penguin.(none)>
2026-06-06 16:06:51 +0100

    feat(nvim): add markdown previewer
    - Use peek.nvim to preview markdown files
    - Add <localleader>p and <localleader>P keymaps to open and close the
    preview respectively

---
+++ nvim/lua/config/functionality/peek.lua
@@ -0,0 +1,32 @@
+local peek
+
+local function find_peek()
+    for _, path in ipairs(vim.opt.rtp:get()) do
+        if path:match("peek.nvim") then
+            return path
+        end
+    end
+end
+
+local keymaps = require("core.lazy_keymaps").new(function()
+    peek = require("peek")
+    peek.setup {
+        app = "browser",
+    }
+
+    local deno_build_command = { "deno", "task", "--quiet", "build:fast" }
+
+    vim.system(deno_build_command, { cwd = find_peek() }, function(res)
+        if res.code ~= 0 then
+            error(("Failed to build peek with code: %d\n%s"):format(res.code, res.stderr))
+        end
+    end)
+end)
+
+keymaps:add("n", "<localleader>p", function()
+    return peek.open
+end, { desc = "Open peek" })
+
+keymaps:add("n", "<localleader>P", function()
+    return peek.close
+end, { desc = "Close peek" })
