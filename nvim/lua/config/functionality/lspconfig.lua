6f5ec1c3744ebfb4965f7ef8a6e62c823556f175
walshfinnian <walshfinnian@penguin.(none)>
2026-06-07 16:41:25 +0100

    feat(nvim): add new lspconfig keymaps
    Use gnd and gnD to goto definition and declaration in a new buffer

--- nvim/lua/config/functionality/lspconfig.lua
+++ nvim/lua/config/functionality/lspconfig.lua
@@ -52,6 +52,16 @@ vim.keymap.set("n", "<leader>c", vim.lsp.buf.code_action, { desc = "Code actions
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
 
+vim.keymap.set("n", "gnd", function()
+    vim.cmd("vs | wincmd l")
+    vim.lsp.buf.definition()
+end, { desc = "Go to definition in a new buffer" })
+
+vim.keymap.set("n", "gnD", function()
+    vim.cmd("vs | wincmd l")
+    vim.lsp.buf.declaration()
+end, { desc = "Go to declaration in a new buffer" })
+
 local function assert_files_written()
     local message_type = "ErrorMsg"
     local file_changes = { { "Open files have changes:", message_type } }
