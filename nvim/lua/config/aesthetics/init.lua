5d700f80d6b2a00cf2c5b5873fc4b248d19762ee
walshfinnian <walshfinnian@penguin.(none)>
2026-06-09 18:54:06 +0200

    chore(nvim): update safe loader
    Use a class approach for the safe loader in order to store state
    information

--- nvim/lua/config/aesthetics/init.lua
+++ nvim/lua/config/aesthetics/init.lua
@@ -1,7 +1,9 @@
-local safe_loader = require("config.safe_loader")
+local safe_loader = require("config.safe_loader").new()
 
-safe_loader.load("config.aesthetics.alpha")
-safe_loader.load("config.aesthetics.bufferline")
-safe_loader.load("config.aesthetics.colorschemes")
-safe_loader.load("config.aesthetics.let-it-snow")
-safe_loader.setup("lualine")
+safe_loader:load("config.aesthetics.alpha")
+safe_loader:load("config.aesthetics.bufferline")
+safe_loader:load("config.aesthetics.colorschemes")
+safe_loader:load("config.aesthetics.let-it-snow")
+safe_loader:setup("lualine")
+
+return safe_loader
--- nvim/lua/config/safe_loader.lua
+++ nvim/lua/config/safe_loader.lua
@@ -1,6 +1,11 @@
 local M = {}
+M.__index = M
 
-function M.load(path)
+function M.new()
+    return setmetatable({ ok = true }, M)
+end
+
+function M:load(path)
     local ok, result = pcall(require, path)
 
     if ok then
@@ -8,10 +13,12 @@ function M.load(path)
     end
 
     vim.notify(("Failed to require `%s` with error:\n%s"):format(path, result), vim.log.levels.ERROR)
+    self.ok = false
+    self.err = self.err or result
 end
 
-function M.setup(plugin_name)
-    local plugin = M.load(plugin_name)
+function M:setup(plugin_name)
+    local plugin = M:load(plugin_name)
 
     if not plugin then
         return
@@ -24,6 +31,8 @@ function M.setup(plugin_name)
     end
 
     vim.notify(("Failed to set up `%s` with error:\n%s"):format(plugin, result), vim.log.levels.ERROR)
+    self.ok = false
+    self.err = self.err or result
 end
 
 return M
