973b95097f30e223eb7514c141e966e05150a94d
walshfinnian <walshfinnian@penguin.(none)>
2026-06-09 18:56:56 +0200

    refactor(nvim): update buffers code
    - Use a utility wipe_undisplayed_buffers function
    - Replace enew keymap
    - Simplify buffer delete all keymap

--- nvim/lua/core/buffers.lua
+++ nvim/lua/core/buffers.lua
@@ -93,7 +93,7 @@ vim.keymap.set("n", "<leader>N]", function()
     end
 end, { desc = "Next buffer " })
 
-vim.keymap.set("n", "<leader>bn", vim.cmd.enew, { desc = "Open a new empty buffer" })
+vim.keymap.set("n", "<leader>w", vim.cmd.enew, { desc = "Open a new empty buffer" })
 
 local discord_buffer, discord_win
 
@@ -166,21 +166,28 @@ local function wipe_buffer(buf)
     vim.cmd("confirm bwipeout " .. buf)
 end
 
-local function delete_undisplayed_buffers()
+---@param filter function A function that returns a truthy value for buffers that should be deleted and a falsy value for buffers that shouldn't
+local function wipe_buffers_filtered(filter)
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if filter(buf) then
+            wipe_buffer(buf)
+        end
+    end
+end
+
+local function wipe_undisplayed_buffers()
     local displayed = {}
 
     for _, win in ipairs(vim.api.nvim_list_wins()) do
         displayed[vim.api.nvim_win_get_buf(win)] = true
     end
 
-    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-        if not displayed[buf] then
-            wipe_buffer(buf)
-        end
-    end
+    wipe_buffers_filtered(function(buf)
+        return not displayed[buf]
+    end)
 end
 
-local function delete_other_buffers()
+local function wipe_other_wins_and_buffers()
     local current_buf = vim.api.nvim_get_current_buf()
     local current_win = vim.api.nvim_get_current_win()
 
@@ -190,34 +197,28 @@ local function delete_other_buffers()
         end
     end
 
-    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-        if buf ~= current_buf then
-            wipe_buffer(buf)
-        end
-    end
+    wipe_buffers_filtered(function(buf)
+        return buf ~= current_buf
+    end)
 end
 
-vim.keymap.set("n", "<leader>bo", delete_undisplayed_buffers, { desc = "Delete undisplayed buffers" })
+vim.keymap.set("n", "<leader>bo", wipe_undisplayed_buffers, { desc = "Delete undisplayed buffers" })
 vim.api.nvim_create_user_command(
     "BufOnly",
-    delete_undisplayed_buffers,
+    wipe_undisplayed_buffers,
     { desc = "Delete undisplayed buffers", bang = true }
 )
 
-vim.keymap.set("n", "<leader>bO", delete_other_buffers, { desc = "Delete other buffers" })
+vim.keymap.set("n", "<leader>bO", wipe_other_wins_and_buffers, { desc = "Delete other buffers" })
 vim.api.nvim_create_user_command(
     "BufCurrentOnly",
-    delete_other_buffers,
+    wipe_other_wins_and_buffers,
     { desc = "Delete all other windows and buffers", bang = true }
 )
 
 vim.keymap.set("n", "<leader>bd", "<cmd>confirm bwipeout<CR>", { desc = "Delete current buffer" })
 
-vim.keymap.set("n", "<leader>bD", function()
-    for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-        wipe_buffer(bufnr)
-    end
-end, { desc = "Delete all buffers" })
+vim.keymap.set("n", "<leader>bD", "<cmd>windo confirm bwipe<CR>", { desc = "Delete all buffers" })
 
 -- Buffer moving
 
