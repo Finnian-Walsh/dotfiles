fb7974ec91fe3101fd85e4618b11bdfbe9265026
walshfinnian <walshfinnian@penguin.(none)>
2026-06-19 16:19:52 +0200

    feat(nvim): replace many plugins with snacks

--- nvim/lua/config/functionality/toggleterm.lua
+++
@@ -1,8 +0,0 @@
-require("lazy_loader")
-    .new(function()
-        require("toggleterm").setup {}
-    end)
-    :map("n", "<leader><CR>", function()
-        return vim.cmd.ToggleTerm
-    end, { desc = "Toggle terminal" })
-    :cmd("ToggleTerm")
