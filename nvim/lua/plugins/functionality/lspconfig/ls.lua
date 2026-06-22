8ff48d1dfefbfa33f48edaa5274263c8f37b8572
Finnian <finnian@archlinux>
2026-06-22 12:30:55 +0100

    refactor(nvim): update lspconfig structure
    Use a directory with different files

---
+++ nvim/lua/plugins/functionality/lspconfig/ls.lua
@@ -0,0 +1,45 @@
+local capabilities = require("blink-cmp").get_lsp_capabilities()
+vim.lsp.config("*", { capabilities = capabilities })
+
+vim.g.rustaceanvim = {
+    server = {
+        settings = {
+            ["rust-analyzer"] = {
+                cargo = { allFeatures = true },
+                checkOnSave = true,
+                check = { command = "clippy" },
+                diagnostics = {
+                    enable = true,
+                    experimental = { enable = true },
+                },
+            },
+        },
+    },
+}
+
+local lua_library = vim.api.nvim_get_runtime_file("", true)
+
+table.insert(lua_library, "${3rd}/luv/library")
+table.insert(lua_library, "${3rd}/luassert/library")
+table.insert(lua_library, "${3rd}/busted/library")
+
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                globals = {
+                    "vim",
+                },
+            },
+            -- workspace = {
+            --     library = lua_library,
+            --     -- checkThirdParty = true,
+            -- },
+        },
+    },
+})
+
+vim.lsp.config("pyright", {})
---
+++ nvim/lua/plugins/functionality/lspconfig/mason.lua
@@ -0,0 +1,39 @@
+require("mason").setup {}
+
+vim.keymap.set("n", "<leader>M", vim.cmd.Mason, { desc = "Open Mason" })
+
+local ensure_installed = {
+    -- Vimscript
+    "vimls",
+    -- Lua
+    "stylua",
+    -- Rust
+    "rust_analyzer",
+    -- Lua
+    "lua_ls",
+    -- Python
+    "pyright",
+    "ruff",
+    -- Asm
+    "asm_lsp",
+    -- Web dev
+    "ts_ls",
+    "html",
+    "cssls",
+    "eslint",
+}
+
+if vim.uv.os_getenv("NO_CLANGD") ~= "1" then
+    table.insert(ensure_installed, "clangd")
+end
+
+vim.api.nvim_create_autocmd("UIEnter", {
+    once = true,
+    callback = function()
+        require("mason-lspconfig").setup {
+            ensure_installed = ensure_installed,
+            automatic_enable = { exclude = { "rust_analyzer" } },
+            -- ignore_install = { "rust_analyzer" },
+        }
+    end,
+})
