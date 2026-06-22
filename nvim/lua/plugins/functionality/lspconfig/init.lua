4b1989672db7985738ad307135917522f3a8a7ea
Finnian <finnian@archlinux>
2026-06-22 12:30:55 +0100

    refactor(nvim): update lspconfig structure
    Use a directory with different files

---
+++ nvim/lua/plugins/functionality/lspconfig/init.lua
@@ -0,0 +1,4 @@
+require("plugins.functionality.lspconfig.mason")
+require("plugins.functionality.lspconfig.format")
+require("plugins.functionality.lspconfig.ls")
+require("plugins.functionality.lspconfig.actions")
