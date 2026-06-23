8c507f6e5b13420fb8a09166a94ebd8366d3f20c
Finnian <finnian@archlinux>
2026-06-23 09:20:55 +0100

    fix(nvim): correct window resizing behavior

--- nvim/lua/plugins/functionality/winresize.lua
+++ nvim/lua/plugins/functionality/winresize.lua
@@ -1,49 +1,56 @@
-local winresize, resize
+local resize
 
 require("lazy_loader")
     .new({
         callback = function()
-            winresize = require("winresize")
-            resize = winresize.resize
+            resize = require("winresize").resize
         end,
     })
     :map("n", "<C-left>", function()
         return function()
             resize(0, 5, "left")
+            vim.cmd.stopinsert()
         end
     end, { desc = "Size window left" })
     :map("n", "<C-down>", function()
         return function()
             resize(0, 5, "down")
+            vim.cmd.stopinsert()
         end
     end, { desc = "Size window down" })
     :map("n", "<C-up>", function()
         return function()
             resize(0, 5, "up")
+            vim.cmd.stopinsert()
         end
     end, { desc = "Size window up" })
     :map("n", "<C-right>", function()
         return function()
             resize(0, 5, "right")
+            vim.cmd.stopinsert()
         end
     end, { desc = "Size window right" })
     :map("n", "<C-S-left>", function()
         return function()
             resize(0, 1, "left")
+            vim.cmd.stopinsert()
         end
     end, { desc = "Size window left fine" })
     :map("n", "<C-S-down>", function()
         return function()
             resize(0, 1, "down")
+            vim.cmd.stopinsert()
         end
     end, { desc = "Size window down fine" })
     :map("n", "<C-S-up>", function()
         return function()
             resize(0, 1, "up")
+            vim.cmd.stopinsert()
         end
     end, { desc = "Size window up fine" })
     :map("n", "<C-S-right>", function()
         return function()
             resize(0, 1, "right")
+            vim.cmd.stopinsert()
         end
     end, { desc = "Size window right fine" })
