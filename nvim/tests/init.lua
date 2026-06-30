f49662efce725936d0d26db7371e1b82200f04f8
walshfinnian <walshfinnian@penguin.(none)>
2026-06-30 20:35:19 +0100

    fix(nvim): correct tests plugins

--- nvim/tests/init.lua
+++ nvim/tests/init.lua
@@ -6,7 +6,8 @@ else
     error(("Cannot run tests (wrong cwd: %s)"):format(cwd))
 end
 
-vim.pack.add { "https://github.com/nvim-mini/mini.test" }
+vim.pack.add(vim.list_extend(require("plugins.spec"), { "https://github.com/nvim-mini/mini.test" }))
+
 require("mini.test").setup {}
 
 MiniTest.run_file("tests/mini_test.lua")
